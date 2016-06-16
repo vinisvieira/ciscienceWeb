2a9f0cbcb733a3f7acefa06a276c9e05c5d1136d
Vinicius <lindemberg.vinicius@gmail.com>
2016-06-16 16:29:45 -0300

    comecando o rest de questoes.

--- src/br/com/ciscience/controll/rest/service/impl/QuestionRestService.java
+++ src/br/com/ciscience/controll/rest/service/impl/QuestionRestService.java
@@ -1,57 +1,56 @@
 package br.com.ciscience.controll.rest.service.impl;
 
-import java.util.List;
-
-import javax.annotation.security.PermitAll;
-import javax.ws.rs.GET;
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
+import br.com.ciscience.model.dao.impl.LevelDAO;
 import br.com.ciscience.model.dao.impl.QuestionDAO;
+import br.com.ciscience.model.entity.impl.Level;
 import br.com.ciscience.model.entity.impl.Question;
 import br.com.ciscience.model.jpa.impl.JPAUtil;
 import br.com.ciscience.util.Constants;
-import br.com.ciscience.util.JSONUtil;
-import br.com.ciscience.util.ResponseBuilderGenerator;
 
 @Path("/question")
 public class QuestionRestService {
 
 	private JPAUtil mSimpleEntityManager;
 	private QuestionDAO mQuestionDAO;
+	private LevelDAO mLevelDAO;
+	
+	@Context
+	private HttpServletRequest servletRequest;
 
-	@GET
-	@PermitAll
-	public Response readQuestions() {
-
+	public Response create(@FormParam("idLevel") String idLevel, @FormParam("text") String text,@FormParam("score") String score){
+		
 		this.mSimpleEntityManager = new JPAUtil(Constants.PERSISTENCE_UNIT_NAME);
-		this.mQuestionDAO = new QuestionDAO(this.mSimpleEntityManager.getEntityManager());
+		this.mQuestionDAO = new QuestionDAO(
+				this.mSimpleEntityManager.getEntityManager());
 		ResponseBuilder responseBuilder = Response.noContent();
 
 		this.mSimpleEntityManager.beginTransaction();
-
+		
 		try {
-
-			List<Question> questions = this.mQuestionDAO.findAll();
-
-			for (Question question : questions) {
-				question.setAlternatives(question.getAlternatives());
+			
+			Question question = new Question();
+			Level level = this.mLevelDAO.getById(Long.parseLong(idLevel));
+			
+			if(level != null && question != null){
+			
+			question.setText(text);
+			question.setScore(Integer.parseInt(score));
+			question.setStatus(Constants.ACTIVE_ENTITY);
+			question.setLevel(level);
+			
 			}
-
-			responseBuilder = ResponseBuilderGenerator.createOKResponseJSON(responseBuilder,
-					JSONUtil.objectToJSON(questions));
-
 		} catch (Exception e) {
-			this.mSimpleEntityManager.rollBack();
-			e.printStackTrace();
-			responseBuilder = ResponseBuilderGenerator.createErrorResponse(responseBuilder);
-		} finally {
-			this.mSimpleEntityManager.close();
+			
 		}
-
+		
 		return responseBuilder.build();
-
 	}
 
 }
