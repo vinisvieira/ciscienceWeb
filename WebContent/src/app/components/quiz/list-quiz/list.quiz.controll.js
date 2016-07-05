ecfa1f6d1dda0fad0170e35807b900386c972273
Danilo Fernando <141220152@aluno.fafica-pe.edu.br>
2016-07-05 20:37:22 -0300

    new-quiz

---
+++ WebContent/src/app/components/quiz/list-quiz/list.quiz.controll.js
@@ -0,0 +1,63 @@
+'use strict';
+app.controller('QuizListCtrl', ['$http', '$location', '$scope','$routeParams', function($http, $location, $scope, $routeParams) {
+
+	var self = this;
+	var appCtrl = $scope.appCtrl;
+	
+	$scope.page = 'list-quiz';
+	
+	$scope.onLoadHtmlFileInNgView = function () {
+		self.listquiz();
+	}
+
+	// ----- Filter List.
+    $scope.filterList = function () {
+    	var arrayFilter = [];
+    	for (var int = 0; int < $scope.initialDataOfGenericList.length; int++) {
+    		var sInput = $scope.filterListInput;
+    			sInput = sInput.toLowerCase();
+			var sComparative = "";
+			sComparative = $scope.initialDataOfGenericList[int].quizList.name;
+			sComparative = sComparative.toLowerCase();
+			if( sComparative.indexOf(sInput) != -1 ){
+	    		arrayFilter.push( $scope.initialDataOfGenericList[int] );
+			}
+		}
+    	$scope.currentPageOfList = 0;
+    	$scope.dataOfGenericList = arrayFilter;
+    }
+    // ----- ./Filter quiz.   
+    	
+	//---- ./List quiz.	
+	self.listquiz = function () {
+		
+		appCtrl.loadSpiner(true);
+
+		$http.get( $scope.applicationUrl + "api/quiz"+"?radom="+Math.random() ).then(function(response) {
+
+			appCtrl.loadSpiner(false);
+
+			var arrayForNgRepeat = response.data;
+			
+			//Ordenar Por Name -- INICIO
+			arrayForNgRepeat.sort(function(a,b) {
+			    if(a.name < b.name) return -1;
+			    if(a.name > b.name) return 1;
+			    return 0;
+			});
+			//Ordenar Por Name -- TÉRMINO
+
+			$scope.dataOfGenericList = arrayForNgRepeat;
+			$scope.initialDataOfGenericList = arrayForNgRepeat;
+			
+
+		}, function(response) {
+			//ERRO
+			appCtrl.loadSpiner(false);
+			alert("Foi Erroo");
+			window.location.href = "#home";
+		});
+	}
+	
+	
+}]);
